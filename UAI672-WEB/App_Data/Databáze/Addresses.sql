643c28886eea4a4d63038bc647e3f40785e6480a
Matys <52925278+Matys134@users.noreply.github.com>
2023-04-12 12:09:52 +0200

    Databáze

---
+++ UAI672-WEB/App_Data/Databáze/Addresses.sql
@@ -0,0 +1,10 @@
+create table Persons.Addresses
+(
+    City   nchar(30),
+    Number int
+)
+go
+
+INSERT INTO UAI672_Database.Persons.Addresses (City, Number) VALUES (N'Č. Budějovice                 ', 122);
+INSERT INTO UAI672_Database.Persons.Addresses (City, Number) VALUES (N'Praha                         ', 12);
+INSERT INTO UAI672_Database.Persons.Addresses (City, Number) VALUES (N'Č. Krumlov                    ', 58);
---
+++ UAI672-WEB/App_Data/Databáze/Details.sql
@@ -0,0 +1,11 @@
+create table Persons.Details
+(
+    Name    nchar(20),
+    Surname nchar(20),
+    ID      int
+)
+go
+
+INSERT INTO UAI672_Database.Persons.Details (Name, Surname, ID) VALUES (N'Jan                 ', N'Petrášek            ', 0);
+INSERT INTO UAI672_Database.Persons.Details (Name, Surname, ID) VALUES (N'Matěj               ', N'Kouba               ', 1);
+INSERT INTO UAI672_Database.Persons.Details (Name, Surname, ID) VALUES (N'Miloš               ', N'Prokýšek            ', 2);
