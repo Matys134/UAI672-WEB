5986e5b68bca0a8e826c43adca2588ac8b68b081
PetrasekJan <petraj14@jcu.cz>
2023-04-18 12:34:03 +0200

    Add contorlers

---
+++ UAI672-WEB/Controllers/AddressesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UAI672_WEB.Models;
+
+namespace UAI672_WEB.Controllers
+{
+    public class AddressesController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: Addresses
+        public ActionResult Index()
+        {
+            return View(db.Addresses.ToList());
+        }
+
+        // GET: Addresses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Addresses addresses = db.Addresses.Find(id);
+            if (addresses == null)
+            {
+                return HttpNotFound();
+            }
+            return View(addresses);
+        }
+
+        // GET: Addresses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Addresses/Create
+        // Chcete-li zajistit ochranu před útoky typu OVERPOST, povolte konkrétní vlastnosti, k nimž 
+        // chcete vytvořit vazbu. Další informace viz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,City,Number")] Addresses addresses)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Addresses.Add(addresses);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(addresses);
+        }
+
+        // GET: Addresses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Addresses addresses = db.Addresses.Find(id);
+            if (addresses == null)
+            {
+                return HttpNotFound();
+            }
+            return View(addresses);
+        }
+
+        // POST: Addresses/Edit/5
+        // Chcete-li zajistit ochranu před útoky typu OVERPOST, povolte konkrétní vlastnosti, k nimž 
+        // chcete vytvořit vazbu. Další informace viz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,City,Number")] Addresses addresses)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(addresses).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(addresses);
+        }
+
+        // GET: Addresses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Addresses addresses = db.Addresses.Find(id);
+            if (addresses == null)
+            {
+                return HttpNotFound();
+            }
+            return View(addresses);
+        }
+
+        // POST: Addresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Addresses addresses = db.Addresses.Find(id);
+            db.Addresses.Remove(addresses);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
